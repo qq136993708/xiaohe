9a5124f3018a9503861d6d7d8e678abbb7fe47dd
qq136993708 <136993708@qq.com>
2017-11-30 18:53:12 +0800

    第二次提交

--- asets/js/index.js
+++ asets/js/index.js
@@ -11,33 +11,54 @@ function change(){
     };
     // 点击
     var flag=true;
-$("#dj").on('click',function(){
-    if (flag) {
-        $(this).find("#yc").hide();
-        $(this).find("#dsj").removeClass(".dsj");
-        $(this).find('#dsj').addClass(".zsj");
-    }else{
-        $(this).find("#yc").show();
-        $(this).find('#dsj').addClass(".zsj");
-        $(this).find("#dsj").removeClass(".dsj");
-    }
-    flag=!flag;
+// $("#dj").on('click',function(){
+//     if (flag) {
+//         $(this).find("#yc").hide();
+//         $(this).find(".dj_show").show();
+//         $(this).find("#sq").show();
+//         $(this).find("#dsj").removeClass(".dsj");
+//         $(this).find('#dsj').addClass(".zsj");
+//     }else{
+//         $(this).find("#yc").show();
+//         $(this).find(".dj_show").hide();
+//         $(this).find('#dsj').addClass(".zsj");
+//         $(this).find("#dsj").removeClass(".dsj");
+//     }
+//     flag=!flag;
     
-});
+// });
 // 产品点击
-$("#cdj").on('click',function(){
-    if (flag) {
-        $(this).find("#cyc").hide();
-        $(this).find("#cdsj").removeClass(".dsj");
-        $(this).find("#dsj").addClass(".zsj");
-        console.log($(this).find('#dsj').position());
-    }else{
-        $(this).find("#cyc").show();
-        $(this).find("#dsj").addClass(".zsj");
-        $(this).find("#cdsj").removeClass(".dsj");
+// $("#cdj").on('click',function(){
+//     if (flag) {
+//         $(this).find("#cyc").hide();
+//         $(this).find("#cdjz").hide();
+//         $(this).find("#sq").show();
+//         $(this).find("#cdsj").removeClass(".dsj");
+//         $(this).find("#dsj").addClass(".zsj");
+//     }else{
+//         $(this).find("#cyc").show();
+//         $(this).find("#sq").show();
+//         $(this).find("#cdjz").hide();
+//         $(this).find("#dsj").addClass(".zsj");
+//         $(this).find("#cdsj").removeClass(".dsj");
 
-    }
-    flag=!flag;
+//     }
+//     flag=!flag;
     
+// });
+$("#dj_show").click(function(){
+    $("#dj").addClass("showInfo")
 });
+$("#sq2").click(function(){
+    // console.log($("#dj").html())
+    $("#dj").removeClass(" showInfo")
+});
+$("#cdjz").click(function(){
+    $("#cdj").addClass("showInfo")
+});
+$("#sq").click(function(){
+    // console.log($("#dj").html())
+    $("#cdj").removeClass(" showInfo")
+});
+
 
